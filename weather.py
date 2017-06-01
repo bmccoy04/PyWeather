d4fe80f452e2cca912d6d8f687e4e6b965c1b186
bmccoy04 <bryanmccoy04@gmail.com>
2017-05-31 19:34:13 -0500

    it works, ish.

--- weather.py
+++ weather.py
@@ -3,13 +3,16 @@ import json
 
 cityState = input("Enter the ciy, state you would like the weather for: ")
 
-print(cityState)
-
 requestUrl = 'http://apidev.accuweather.com/locations/v1/search?q=' + cityState + '&apikey=hoArfRosT1215'
 
 req = requests.get(requestUrl)
 
-data = req.json()
-
-for d in data:
-    print(d)
+if req.status_code == 200:
+    data = req.json()
+    for d in data:  
+        s = 'http://apidev.accuweather.com/currentconditions/v1/' + d['Key'] + '.json?language=en&apikey=hoArfRosT1215'        
+        weather = requests.get(s)
+        dataTwo = weather.json()
+        for dTwo in dataTwo:
+            print('It is ' + dTwo['WeatherText'] + ' outside.')
+            print('Temp is: ' + str(dTwo['Temperature']['Imperial']['Value']))
